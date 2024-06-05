432d18307bde85726b16fdcb5015690ae0524a0e
Steve Carmichael <steve@techelevator.com>
2024-06-05 15:30:17 -0400

    added capstone starter code

---
+++ src/main/java/com/techelevator/Application.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Application application = new Application();
+		application.run();
+	}
+
+	public void run() {
+
+		//do amazing stuff here!
+
+	}
+}
